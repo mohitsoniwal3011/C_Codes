623fda1ca3ea2f235a57ed0bc2fb57ce53fbf448
mohitsoniwal3011 <mohitsoniwal3011@gmail.com>
2020-08-30 14:25:59 +0530

    programe to calculate the area of tringle

---
+++ Basic_codes/area_of_tringle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int  main ()
+{
+	float base,height,area ;
+	printf("enter the base and height respectively \n");
+	scanf("%f%f",&base,&height);
+	area=0.5*base*height;
+	printf("the area of the tringle with the given base and height is %f \n",area);
+	return 0;
+}
+
+
+ 
